74b452a72f8f6829e3639ff2ef63c74fc74fe612
alessandraciccone <alessandraciccone@icloud.com>
2026-01-08 10:28:32 +0100

    adds footer

--- habitT/src/App.jsx
+++ habitT/src/App.jsx
@@ -1,23 +1,27 @@
-
 import Navbar from "./components/Navbar";
 import Registrazione from "./components/Registrazione";
 import Login from "./components/Login";
+import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div
-        className="min-h-screen bg-green-100"
+        className="min-h-screen flex flex-col bg-green-100"
         style={{ fontFamily: "Pacifico, cursive" }}
       >
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Registrazione />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Registrazione />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
 }
+
 export default App;
---
+++ habitT/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="w-full py-4 mt-8 text-center text-pink-400 font-serif text-lg shadow-inner">
+    © 2026 Piccoli Ritmi. Tutti i diritti riservati.
+  </footer>
+);
+
+export default Footer;
